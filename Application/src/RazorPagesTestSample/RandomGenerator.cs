5d5bc52fcd0a36028941acde583fcf268d849d5c
System Administrator <admin@mngenvmcap728493.onmicrosoft.com>
2024-02-15 15:43:29 -0600

    Yadda, yadda, Resolves issue #7

---
+++ Application/src/RazorPagesTestSample/RandomGenerator.cs
@@ -0,0 +1,9 @@
+// // Write a loop from 0 to 10 and print out the numeric value of the iterator for each loop iteration.
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+// ```
+
+// ###
+
