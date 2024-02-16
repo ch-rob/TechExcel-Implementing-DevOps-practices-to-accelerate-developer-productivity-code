9ea1a35dc6c7bb88fc67eb5913e64e4919e344a9
Chad Voelker <chad.voelker@gmail.com>
2024-02-16 14:32:19 -0600

    Update Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
-// Test to deply docker
+// Test to deply docker with .net 8.0
 namespace RazorPagesTestSample
 {
     public class Program
